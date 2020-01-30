5feabd8f04809ce61ecf150704806837631bb34e
simona <palumbosimona118@gmail.com>
2020-01-30 15:43:32 +0100

    ex ok

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
   var numeroFisso = 4;
 
-  $('.square').click(function () {
+  $('.square').click(function ()
+    
+   {
+     var square = $(this);
     $.ajax(
       {
         url: "https://flynn.boolean.careers/exercises/api/random/int",
         method: "GET",
         success: function (data, stato) {
-          console.log(data.response);
+          numeroFisso = data.response;
+          square.text(numeroFisso);
+          if (numeroFisso <= 5) {
+            square.addClass('yellow');
+          }
+          else {
+            square.addClass('green');
+          }
+
+        //  console.log(data.response);
+
         },
         error: function (richiesta, stato, errori) {
           alert("Number error" + errore);
         }
       }
     );
-    $(this).text(numeroFisso);
-    if (numeroFisso <= 5) {
-      $(this).addClass('yellow');
-    }
-    else {
-      $(this).addClass('green');
-    }
+
   });
 
 });
